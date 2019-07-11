704b576f53bf51d546fd11c5717fff25bf52d033
ahrooran <ahrooran@hotmail.co.uk>
2019-07-11 11:15:02 +0100

    Add files via upload

---
+++ exporting.js
@@ -0,0 +1,10 @@
+const multiply = require('./Maths.js')
+multiply.multiplyByTwo(10)
+
+
+
+const divideByTwo = require('./Maths.js').divideByTwo
+divideByTwo(18)
+
+console.log(multiply.multiplyByTwo(10));
+console.log(divideByTwo(18));
